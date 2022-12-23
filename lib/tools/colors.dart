fc2411895f6d370cc8f84c1d408d8117279c6da6
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-12-23 15:10:51 +0330

    flutter_travel_app

---
+++ lib/tools/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffF4664D);
+const backgroundColor = Color(0xffFCFCFC);
+const shadowColor = Color(0xFFE0E0E0);
+const darkColor = Color(0xFF212121);
+const whiteColor = Color(0xffFFFFFF);
+
+const color1 = Color(0xffEE6D4F);
+const color2 = Color(0xff6168E6);
+const color3 = Color(0xffFF9156);
+const color4 = Color(0xff9571F6);
