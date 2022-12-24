2cf6d5cb522fbf55a18c5a50e552a503ab68d887
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-12-24 14:05:12 +0330

    flutter_travel_app

---
+++ lib/tools/styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const textStyle1=TextStyle(color: darkColor,fontSize:24,fontFamily: 'bold' );
+const textStyle2=TextStyle(color: darkColor,fontSize:22,fontFamily: 'bold' );
+const textStyle3=TextStyle(color: darkColor,fontSize:20,fontFamily: 'regular' );
+const textStyle4=TextStyle(color: darkColor,fontSize:18,fontFamily: 'regular' );
+const textStyle12=TextStyle(color: darkColor,fontSize:16,fontFamily: 'bold' );
+const textStyle5=TextStyle(color: darkColor,fontSize:14,fontFamily: 'regular' );
+//
+const textStyle6=TextStyle(color: whiteColor,fontSize:18,fontFamily: 'bold' );
+const textStyle7=TextStyle(color: whiteColor,fontSize:16,fontFamily: 'regular' );
+const textStyle8=TextStyle(color: whiteColor,fontSize:14,fontFamily: 'regular' );
+//
+const textStyle9=TextStyle(color: Color(0xFF757575),fontSize:14,fontFamily: 'regular' );
+const textStyle13=TextStyle(color: Color(0xFF757575),fontSize:16,fontFamily: 'bold' );
+//
+const textStyle10=TextStyle(color: primaryColor,fontSize:18,fontFamily: 'regular' );
+const textStyle11=TextStyle(color: primaryColor,fontSize:16,fontFamily: 'regular' );
