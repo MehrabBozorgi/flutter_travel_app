49e81e2b4d383437c581c170cf1526402f769521
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-12-24 14:04:52 +0330

    flutter_travel_app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_app/screen/second_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SecondScreen(),
+    );
+  }
+}
